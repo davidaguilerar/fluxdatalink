151fb1fcfef1636b8ba639bde5acee4d3fbdf977
David Aguilera-Riquelme <david.aguilerar@yahoo.com>
2019-10-15 00:27:18 -0300

    Changes for ESP8266

--- fluxdatalink/fluxdatalink.ino
+++ fluxdatalink/fluxdatalink.ino
@@ -1,158 +1,195 @@
 /*
- * LiCor GasAnalyzer Serial to Bluetooth/USB converter with addtitinal sensor input. 
- * To be used with FluxPuppy App.
- *
- * This program is written to be used with an ESP32 OLED development board.
- * David Basler, 2018
- */
+   LiCor GasAnalyzer Serial to Bluetooth/USB converter with addtitinal sensor input.
+   To be used with FluxPuppy App.
+
+   This program is written to be used with an ESP32 OLED development board.
+   David Basler, 2018
+
+   Modified by: David Aguilera-Riquelme
+   Changes:
+    - Support for two ADS1115 analog-to-digital converters, used to measure PAR, Air Temperature and Canopy Temperature
+    - Changed to NodeMCU Amica microcontroller, based on ESP8266 + HC06 Bluetooth dongle
+*/
 
 /*
- * ESP32 OLED
- * 
- * PINOUT:
- * 5 SDA   ----   SDA BME280
- * 4 SCL   ----   SCL BME280
- * 
- * 14 RX   ----   TX S8
- * 15 TX   ----   RX S8
- * 
+   ESP8266
 
- */
+   PINOUT:
 
-#define DEVICENAME "--- DaBa FLUX ---"
-#define DISPLAY  // comment out if no display is connected
+   14 RX   ----   TX S8
+   15 TX   ----   RX S8
 
-//#################### DISPLAY
-#ifdef DISPLAY
-  #include "SSD1306Wire.h" // inclues a call to Wire.h
-  SSD1306Wire  display(0x3c, 5, 4);
-#else
-  #include "Wire.h"
-#endif
-//#################### BLUETOOTH 
-#include "BluetoothSerial.h"
-#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
-#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
-#endif
-BluetoothSerial SerialBT;
 
-//####################  LICOR Serial connection
+*/
 
-HardwareSerial Serial1(1); // GPIO 14/15 
+#include "Wire.h"
+#include "HardwareSerial.h"
+#include <ESP8266WiFi.h>
+#include <Ticker.h>
+#include <Adafruit_ADS1015.h>
 
-//#################### BME280 ENVIRON SENSOR
-#include "cactus_io_BME280_I2C.h"
-#define BME_ADDRESS 0x76
-BME280_I2C bme(BME_ADDRESS);  // I2C using address 0x76
-bool bmeAvail=false;
+//#################### Serial connection
+
+//HardwareSerial Serial1(1); // for LI-840A
+HardwareSerial SerialBT(2); // for HC06
+
 //#################### GLOBAL CONTROL VRIABLES
 
 // Loop interrupts
-bool HzInterrupt=false;
+bool HzInterrupt = false;
 bool new_data_ready = false;
-
-// Progressbars
-bool warmup=false;
-int progress=0;
-int progressmax;
 
 // Serial buffers
 char inbuffer_licor[500];      // For Licor ON Serial1
-int sbl=0;
-char FPbuffer [600]; // Final Sting to be submittet to Flux Puppy
+int sbl = 0;
+char FPbuffer [620]; // Final Sting to be submitted to Flux Puppy
 char ExtraBuffer[255];
 int len;
 
-*/
 // String parser
 int data[25];
 int datalen[25];
 int tags [25];
 int taglen[25];
 
-#ifdef DISPLAY
-// Current Values for display
-float co2value=NAN;   // FROM LiCor
-float h2ovalue=NAN;   // FROM LiCor
+// Current Values
+float co2value = NAN; // FROM LiCor
+float h2ovalue = NAN; // FROM LiCor
+float pressvalue = NAN; // FROM LiCor (Cell Pressure)
 
-float rhvalue=NAN;    // FROM BME280
-float tempvalue=NAN;  // FROM BME280
-float pressvalue=NAN; // FROM BME280
-#endif
+float tempvalue = NAN; // FROM 107 temperature probe
+float parvalue = NAN; // FROM LI-190R
+float tptempvalue = NAN; // FROM SI-111 (thermopile)
+float tstempvalue = NAN; // FROM SI-111 (thermistor)
+float canopyvalue = NAN; //FROM SI-111 and computed
+
 
 //#################### 1HZ INTERRUPT
-
-volatile int interruptCounter;
-int totalInterruptCounter;
- 
-hw_timer_t * timer = NULL;
-portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
- 
-void IRAM_ATTR onTimer() {
-  portENTER_CRITICAL_ISR(&timerMux);
-  HzInterrupt=true;
-  portEXIT_CRITICAL_ISR(&timerMux);
+Ticker timer1;
+void itsasecond() {
+  HzInterrupt = true;
 }
-
 
 //#################### EXTRA SENSOR DATA , integration in data string for FLUX PUPPY
+//
+#define ADS_1 0x49
+#define ADS_2 0x48
 
-// ################# BME280
+Adafruit_ADS1115 ads1(ADS_1);  // construct an ads1115 at address 0x49
+Adafruit_ADS1115 ads2(ADS_2);  // construct an ads1115 at address 0x48
 
-bool i2cReady(uint8_t adr){
-uint32_t timeout=millis();
-bool ready=false;
-while((millis()-timeout<100)&&(!ready)){
-  Wire.beginTransmission(adr);
-  ready=(Wire.endTransmission()==0);
+ads1.setGain(GAIN_SIXTEEN); // any other measurement
+ads2.setGain(GAIN_ONE);  // this is for thermistor
+
+float scaleads1 = 0.0078125; // mV/number
+float scaleads2 = 0.125; // mV/number
+
+bool i2cReady(uint8_t adr) {
+  uint32_t timeout = millis();
+  bool ready = false;
+  while ((millis() - timeout < 100) && (!ready)) {
+    Wire.beginTransmission(adr);
+    ready = (Wire.endTransmission() == 0);
   }
-return ready;
+  return ready;
 }
+
+float volt107   = NAN;   //107 temperature probe
+float voltsi111a = NAN;  //thermopile for SI-111
+float voltsi111b = NAN; //thermistor for SI-111
+float voltli190r = NAN; //LI-190R PAR radiometer
+float refvolt = 3.3; // 3.3V applied to ADS1115
+
+//################### CALIBRATION CONSTANTS FOR SPECIFIC SENSORS, OBTAINED FROM ITS FACTORY CALIBRATION SHEET
+// THIS CONSTANTS ARE UNIQUE FOR EACH SENSOR. YOU MUST EDIT THIS PART OF CODE
+float licor_calib = 245.42; //calibration constant for LI-190R Serial Number Q105952, connected with an 2290 Millivolt Adapter, units: umol/s/m^2 /mV
+float m0 = 1313790000 ; float m1 = 7203270; float m2 = 56191.3;    // calibration polynomial coefficients for SI-111 Serial Number 6798
+float b0 = -8736140 ; float b1 = 146949; float b2 = 3329.97;    // calibration polynomial coefficients for SI-111 Serial Number 6798
+
+
+////SI111
+//  m = 1313790000 +( 7203270*Temperatura_Si111) + (56191.3*Temperatura_Si111^2)  ' Pendiente de calculo de temperatura de objeto
+//    b = -8736140 + (146949*Temperatura_Si111) + (3329.97*Temperatura_Si111^2)     ' Interseccion con el eje y para calcular Tbody
+//    ' Calculo de temperatura de objeto usando m y b
+//    SBT_K = Temperatura_Si111+273.15
+//    TT_K = SBT_K^4 + TTmV*m +b
+//    TT_K = SQR(SQR(TT_K))
+//    Temperatura_Canopia = TT_K-273.15                   ' Temperatura de Canopia en grados Celsius
 
 void get_environ_data() {
-  rhvalue=NAN;    
-  tempvalue=NAN;
-  pressvalue=NAN; 
-  if (i2cReady(BME_ADDRESS)){
-        bme.readSensor(); 
-        tempvalue =  bme.getTemperature_C();// °C
-        pressvalue = bme.getPressure_MB();  // mbar"
-        rhvalue = bme.getHumidity();       // %
+  // get enviromental values from sensors
+
+  tempvalue = NAN; // FROM 107 temperature probe
+  parvalue = NAN; // FROM LI-190R
+  tptempvalue = NAN; // FROM SI-111 (thermopile)
+  tstempvalue = NAN; // FROM SI-111 (thermistor)
+  canopyvalue = NAN; // FROM SI-111 and computed
+
+  if (i2cReady(ADS_1) && i2cReady(ADS_2)) {
+    // read data from ADC
+
+
   }
 }
 
-void getExtraStr(){  
-  char hbuffer[7];
+void getExtraStr() {
   char tbuffer[7];
   char pbuffer[7];
-  double nodata=-999.0;
+  char parbuffer[7];
+  char ctbuffer[7];
+
+  double nodata = -999.0;
   // FORMAT STRINGS
-  ExtraBuffer[0]='\0';
-  if (isnan(rhvalue))  {dtostrf(nodata, 3, 0, hbuffer);}  else { dtostrf(rhvalue, 3, 1, hbuffer);}  
-  if (isnan(tempvalue)) {dtostrf(nodata, 3, 0, tbuffer);} else  { dtostrf(tempvalue, 3, 1, tbuffer);}
-  if (isnan(pressvalue)){dtostrf(nodata, 3, 0, pbuffer);}  else { dtostrf(pressvalue, 3, 1, pbuffer);}
-  sprintf(ExtraBuffer,"<tair>%se0</tair><pair>%se0</pair><hair>%se0</hair>",tbuffer,pbuffer,hbuffer);
+  ExtraBuffer[0] = '\0';
+  if (isnan(tempvalue)) {
+    dtostrf(nodata, 3, 0, tbuffer);
+  } else  {
+    dtostrf(tempvalue, 3, 1, tbuffer);
   }
+  if (isnan(pressvalue)) {
+    dtostrf(nodata, 3, 0, pbuffer);
+  }  else {
+    dtostrf(pressvalue, 3, 1, pbuffer);
+  }
+  if (isnan(parvalue)) {
+    dtostrf(nodata, 3, 0, parbuffer);
+  }  else {
+    dtostrf(parvalue, 3, 1, parbuffer);
+  }
+  if (isnan(canopyvalue)) {
+    dtostrf(nodata, 3, 0, ctbuffer);
+  }  else {
+    dtostrf(canopyvalue, 3, 1, ctbuffer);
+  }
+  sprintf(ExtraBuffer, "<tair>%se0</tair><pair>%se0</pair><par>%se0</par><tveg>%se0</tveg>", tbuffer, pbuffer, parbuffer, ctbuffer);
+}
 
 
 //#################### PARSE LICOR DATA
 
-double sctof (char * value){ //scientific notation to double e.g 123.4e2
-    char num[10];
-    char ep[3];
-    int i=0,j=0;
-    while (value[i]!='e') {num[i]=value[i]; i++;}
-    num[i]='\0';
+double sctof (char * value) { //scientific notation to double e.g 123.4e2
+  char num[10];
+  char ep[3];
+  int i = 0, j = 0;
+  while (value[i] != 'e') {
+    num[i] = value[i];
     i++;
-    while (value[i]) {ep[j]=value[i]; i++; j++;}
-    ep[j]='\0';
-    return (atof(num)*pow(10.0,atof(ep)));
+  }
+  num[i] = '\0';
+  i++;
+  while (value[i]) {
+    ep[j] = value[i];
+    i++;
+    j++;
+  }
+  ep[j] = '\0';
+  return (atof(num) * pow(10.0, atof(ep)));
 }
 
-void getstring(char * data, int start ,int len, char * out){
-  for (int i=0;i<len;i++) out[i]= data[i+start];
-  out[len]='\0';
-  }
+void getstring(char * data, int start , int len, char * out) {
+  for (int i = 0; i < len; i++) out[i] = data[i + start];
+  out[len] = '\0';
+}
 
 void piggyback_licor_string() {
   // Splits xml structures in tags and elements
@@ -161,19 +198,19 @@ void piggyback_licor_string() {
   int bl = strlen(inbuffer_licor);
   int tp = 0;
   int x = -1;
-  int i,j,dp;
+  int i, j, dp;
   // split xml String into tags and data (position and length)
   for (i = 0; i < bl; i++) {         // find
     if (inbuffer_licor[i] == '<') {
-      if (x >= 0) datalen[x]= tp;    // finalize data
+      if (x >= 0) datalen[x] = tp;   // finalize data
       x++;
       tp = 0;
-      tags[x]= i;                    // Start new tag
+      tags[x] = i;                   // Start new tag
       tp++;
     } else if (inbuffer_licor[i] == '>') {
-      taglen[x]=tp+1;                // finalize tag
+      taglen[x] = tp + 1;            // finalize tag
       tp = 0;
-      if (i < (bl-1)) data[x]= i+1;  // Start new data
+      if (i < (bl - 1)) data[x] = i + 1; // Start new data
     } else {
       tp++;
     }
@@ -181,173 +218,85 @@ void piggyback_licor_string() {
   x++;
 
   /* //DEBUG
-  Serial.println(inbuffer_licor);
-  for (i=0;i<x;i++){Serial.print(tags[i]);Serial.print(":");Serial.println(taglen[i]);getstring(inbuffer_licor,tags[i],taglen[i],tagstring);Serial.print(tagstring);Serial.print(" -----> ");Serial.print(data[i]);Serial.print(":");Serial.println(datalen[i]);getstring(inbuffer_licor,data[i],datalen[i],datastring);Serial.print(datastring);}
+    Serial.println(inbuffer_licor);
+    for (i=0;i<x;i++){Serial.print(tags[i]);Serial.print(":");Serial.println(taglen[i]);getstring(inbuffer_licor,tags[i],taglen[i],tagstring);Serial.print(tagstring);Serial.print(" -----> ");Serial.print(data[i]);Serial.print(":");Serial.println(datalen[i]);getstring(inbuffer_licor,data[i],datalen[i],datastring);Serial.print(datastring);}
   */
-  
+
   // Test input string is valid (cointains <data> tag in as second element)
-  getstring(inbuffer_licor,tags[1],taglen[1],tagstring);
-  if (strcmp(tagstring,"<data>")) {FPbuffer[0]='\0';return;}
-  
-  getExtraStr();  
+  getstring(inbuffer_licor, tags[1], taglen[1], tagstring);
+  if (strcmp(tagstring, "<data>")) {
+    FPbuffer[0] = '\0';
+    return;
+  }
+
+  getExtraStr();
 
 
   /// Now set the values for display
-  for (i=2;i<x;i++){
-    getstring(inbuffer_licor,tags[i],taglen[i],tagstring);
-    getstring(inbuffer_licor,data[i],datalen[i],datastring);
-    if (!strcmp(tagstring,"<raw>")){ // stop searching when you reach the raw tag
+  for (i = 2; i < x; i++) {
+    getstring(inbuffer_licor, tags[i], taglen[i], tagstring);
+    getstring(inbuffer_licor, data[i], datalen[i], datastring);
+    if (!strcmp(tagstring, "<raw>")) { // stop searching when you reach the raw tag
       break;
-    }else if (!strcmp(tagstring,"<co2>")){
-      co2value=sctof(datastring);
-    }else if (strcmp(tagstring,"<h2o>")==0){
-      h2ovalue=sctof(datastring);
+    } else if (!strcmp(tagstring, "<co2>")) {
+      co2value = sctof(datastring);
+    } else if (strcmp(tagstring, "<h2o>") == 0) {
+      h2ovalue = sctof(datastring);
+    } else if (strcmp(tagstring, "<cellpres>") == 0) {
+      pressvalue = sctof(datastring);
     }
   }
-  
- // Now put the string back together adding sensor data
-  FPbuffer[0]= '\0';
-  getstring(inbuffer_licor,tags[0],taglen[0],tagstring);
-  strcpy(FPbuffer,tagstring);        // <device> tag
-  getstring(inbuffer_licor,tags[1],taglen[1],tagstring);
-  strcat(FPbuffer,tagstring);        // <data> tag
-  strcat(FPbuffer,ExtraBuffer); 
-  strcat(FPbuffer,&inbuffer_licor[tags[2]]);
-  strcat(FPbuffer,"\n");
+
+  // Now put the string back together adding sensor data
+  FPbuffer[0] = '\0';
+  getstring(inbuffer_licor, tags[0], taglen[0], tagstring);
+  strcpy(FPbuffer, tagstring);       // <device> tag
+  getstring(inbuffer_licor, tags[1], taglen[1], tagstring);
+  strcat(FPbuffer, tagstring);       // <data> tag
+  strcat(FPbuffer, ExtraBuffer);
+  strcat(FPbuffer, &inbuffer_licor[tags[2]]);
+  strcat(FPbuffer, "\n");
 }
 
-#ifdef DISPLAY
-//#################### DISPLAY DATA
-
-void show_all(){
- // clear the display
-  char sbuffer[5];
-  char cbuffer[7];
-  char hbuffer[10];
-  char tbuffer[10];
-  char pbuffer[10];
-  char timestr[20];
-  display.clear();
-  
-  if (warmup){
-    drawProgressBar("WELCOME");
-    display.display();  
-    return;
-    }
-    
-  if (isnan(co2value))  { strcpy(cbuffer,"----.-");}  else { dtostrf(co2value,   3, 1, cbuffer);}  //value, mininum width, precision, buffer
-  if (isnan(rhvalue))   { strcpy(hbuffer,  "--.-");}  else { dtostrf(rhvalue,    3, 1, hbuffer);strcat (hbuffer," %");}
-  if (isnan(tempvalue)) { strcpy(tbuffer,  "--.-");}  else { dtostrf(tempvalue,  3, 1, tbuffer);strcat (tbuffer," °C");}
-  if (isnan(pressvalue)){ strcpy(pbuffer,"----.-");}  else { dtostrf(pressvalue, 3, 1, pbuffer);strcat (pbuffer," mb");}
-  int shifty=0;
- 
-  display.setTextAlignment(TEXT_ALIGN_LEFT);
-  display.setFont(ArialMT_Plain_10);
-  display.drawString(0, 3,"FLUX PUPPY" );
-
-  display.setTextAlignment(TEXT_ALIGN_RIGHT);
-  display.setFont(ArialMT_Plain_24);
-  display.drawString(72, 20+shifty,cbuffer);
-  display.setTextAlignment(TEXT_ALIGN_LEFT);
-  display.setFont(ArialMT_Plain_16);
-  display.drawString(74, 26+shifty, "ppm");
-
-  display.setFont(ArialMT_Plain_10);
-  display.setTextAlignment(TEXT_ALIGN_LEFT);
-  display.drawString(0, 52,tbuffer);
-
-  display.setTextAlignment(TEXT_ALIGN_CENTER);
-  display.drawString(58, 52,hbuffer);
-
-  display.setTextAlignment(TEXT_ALIGN_RIGHT);
-  display.drawString(128, 52, pbuffer);
-  display.display();  
-  }
-
-void drawProgressBar(char * title) { 
-  double slope = 100.0 / progressmax;
-  int cv =  slope * progress;
-  display.setTextAlignment(TEXT_ALIGN_CENTER); 
-  display.drawString(64, 0,DEVICENAME);
-  //drawProgressBar(x, y, width, height, value);
-  display.drawProgressBar(10, 32, 100, 10, cv);
-  display.drawString(64, 15, String(cv) + "%");
-  display.drawString(64, 50, title);  
-}
-
-#endif
 //#################### SETUP
 
 void setup() {
-  
-    //Serial port USB
-    Serial.begin(9600);
-    //Bluetooth  
-    SerialBT.begin("DaBaFlux_01"); //Bluetooth device name
-    //LICOR
-    Serial1.begin(9600,SERIAL_8N1,15, 14);
 
-#ifdef Display
-    //Display
-    display.init();
-    display.setContrast(255);
-    display.flipScreenVertically();
-#endif
-    
-    //Timer
-    timer = timerBegin(0, 80, true);
-    timerAttachInterrupt(timer, &onTimer, true);
-    timerAlarmWrite(timer, 1000000, true);
-    timerAlarmEnable(timer);
+  //Serial port USB
+  Serial.begin(9600);
+  //Bluetooth HC06
+  SerialBT.begin(9600);
+  //LICOR
+  Serial1.begin(9600);
 
-    // BME280
-    if (bme.begin()) {
-      bmeAvail=true;
-      bme.setTempCal(-1);
-    } //  else {Serial.println("Could not find a valid BME280 sensor, check wiring!");}
+  //Timer
+  timer1.attach(1, itsasecond);
 
-# ifdef
-    //WARMUP
-    warmup=true;
-    progressmax=50;
-    progress=0;
-    for (int i=0;i<50;i++){
-      progress++;
-      show_all();
-      delay(20);
-      }
-    warmup=false;
 }
 
 //#################### MAIN LOOP
-int data_ready=0;
+int data_ready = 0;
 
 void loop() {
-
   if (HzInterrupt) {
-      if (bmeAvail) get_environ_data();
-      show_all();
-      HzInterrupt=false;
-    }
- 
+    get_environ_data();
+    HzInterrupt = false;
+  }
   // CHECK FOR INCOMING SERIAL DATA FROM LICOR
-   while (Serial1.available()){
-      char c = Serial1.read();
-      if (c =='\n'){
-       inbuffer_licor[sbl]='\0';
-       piggyback_licor_string(); // puts data in FPbuffer
-       SerialBT.print(FPbuffer);
-       Serial.print(FPbuffer);
-       //warmup=false;
-       show_all();
-       sbl=0;
-       inbuffer_licor[sbl]='\0';        
-      }else {
-       inbuffer_licor[sbl]=c;
-       sbl++;
-      }
+  while (Serial1.available()) {
+    char c = Serial1.read();
+    if (c == '\n') {
+      inbuffer_licor[sbl] = '\0';
+      piggyback_licor_string(); // puts data in FPbuffer
+      SerialBT.print(FPbuffer);
+      Serial.print(FPbuffer);
+      //warmup=false;
+      sbl = 0;
+      inbuffer_licor[sbl] = '\0';
+    } else {
+      inbuffer_licor[sbl] = c;
+      sbl++;
     }
-    
+  }
   delay(20);
 }
-
